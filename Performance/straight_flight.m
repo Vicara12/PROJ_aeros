50ff23de38a91fbcccd498835734391f4148c02a
francesc14g <fran.14hg@gmail.com>
2021-10-09 11:50:52 +0200

    ASCENT MANEUVER added

--- Performance/straight_flight.m
+++ Performance/straight_flight.m
@@ -1,4 +1,4 @@
-function [x,h,v,Cl,Cd] = straight_flight(rho,T,g,S,Cd0,k,W,gamma,UP,right,time,BC)
+function [x,h,v,Cl,Cd] = straight_flight(T,g,S,Cd0,k,W,gamma,UP,right,time,BC)
 
 % Solve differential equation system
 %X(1) = x, X(2) = h, X(3) = v
@@ -6,11 +6,11 @@ function [x,h,v,Cl,Cd] = straight_flight(rho,T,g,S,Cd0,k,W,gamma,UP,right,time,B
 if right == 1
     f = @(tv,X) [ X(3)*cos(gamma);
         X(3)*sin(gamma);
-        g/W*(T-0.5*rho*X(3)^2*S*(Cd0+k*(-W.*cos(gamma) ./ (0.5*rho*X(3)^2*S))^2) - W*sin(gamma))];
+        g/W*(T-0.5*atmos(X(2))*X(3)^2*S*(Cd0+k*(-W.*cos(gamma) ./ (0.5*atmos(X(2))*X(3)^2*S))^2) - W*sin(gamma))];
 elseif right == 0
     f = @(tv,X) [ -X(3)*cos(gamma);
         X(3)*sin(gamma);
-        g/W*(T-0.5*rho*X(3)^2*S*(Cd0+k*(-W.*cos(gamma) ./ (0.5*rho*X(3)^2*S))^2) - W*sin(gamma))];
+        g/W*(T-0.5*atmos(X(2))*X(3)^2*S*(Cd0+k*(-W.*cos(gamma) ./ (0.5*atmos(X(2))*X(3)^2*S))^2) - W*sin(gamma))];
 end
 
 [t,X]=ode45(f,time,BC);
@@ -18,11 +18,16 @@ x(:,1) = X(:,1);
 h(:,1) = X(:,2);
 v(:,1) = X(:,3);
 
+% Density evolution
+for i = 1:length(h)
+    rho(i,1) = atmos(h(i));
+end
+
 % Cl calculation
 if UP == 1
-    Cl(:,1) = W.*cos(gamma) ./ (0.5*rho.*v.^2*S);
+    Cl(:,1) = W.*cos(gamma) ./ (0.5.*rho.*v.^2*S);
 elseif UP == 0
-    Cl(:,1) = -W.*cos(gamma) ./ (0.5*rho.*v.^2*S);
+    Cl(:,1) = -W.*cos(gamma) ./ (0.5.*rho.^2*S);
 end
 
 % Cd and thrust calculation
